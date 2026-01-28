188de05afd80eaca658543a24c3ac997c311b37a
CodewithMukal <mukal.github@gmail.com>
2026-01-28 21:09:06 +0530

    DB: Added subcategories and seeded

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,10 +2,18 @@ import prisma from "@/lib/prisma";
 
 async function main(){
     const catgories = ["Bottles", "Pouches","Bags","Cardboard Boxes"]
+    const subcategories = [
+                           ["Plastic","Glass","Aluminum","PLA"]
+                           ,["Plastic","Aluminum","Laminated Paper Based"]
+                          ,["Plastic","Paper","Jute","Cloth","Woven","Non Woven","Compostable"]
+                          ,["Single Wall","Double Wall","Triple Wall","Duplex Boxes"]
+                        ]
     for(const name of catgories){
         await prisma.category.upsert({
             where: {name: name},
-            update: {},
+            update:{
+                subcategories: subcategories[catgories.indexOf(name)]
+            },
             create: {name: name}
         })
     }
