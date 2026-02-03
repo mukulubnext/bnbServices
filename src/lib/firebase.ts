5d9fe5b99a91db40d85754ed8359f665ba7dcbcb
CodewithMukal <mukal.github@gmail.com>
2026-02-03 19:29:37 +0530

    fix:Changed to .env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyAD9aKZ1I02DEAlbuBiEizdPtwoV4Nnaec",
-  authDomain: "bnbservices-600a1.firebaseapp.com",
-  projectId: "bnbservices-600a1",
-  storageBucket: "bnbservices-600a1.firebasestorage.app",
-  messagingSenderId: "704488998770",
-  appId: "1:704488998770:web:1d5566f8199b31be027feb"
+  apiKey: (process.env.NEXT_PUBLIC_FIREBASE_API_KEY),
+  authDomain:(process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN),
+  projectId: (process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID),
+  storageBucket: (process.env.NEXT_PUBLIC_FIREBASE_STORAGE),
+  messagingSenderId: (process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID),
+  appId: (process.env.NEXT_PUBLIC_FIREBASE_APP_ID),
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
