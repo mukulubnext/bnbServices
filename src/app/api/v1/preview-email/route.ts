9132a70a5b2b1796a37e4caa5e59cab39cee0688
CodewithMukal <mukal.github@gmail.com>
2026-02-06 21:31:45 +0530

    UI: Fixed otp mail UI

--- src/app/api/v1/preview-email/route.ts
+++ src/app/api/v1/preview-email/route.ts
@@ -1,12 +1,10 @@
 import { NextResponse } from "next/server";
-import { renderForgotPasswordTemplate } from "@/lib/mail";
+import { renderForgotPasswordTemplate, renderOtpTemplate } from "@/lib/mail";
 
 export async function GET() {
-  const html = await renderForgotPasswordTemplate({
+  const html = await renderOtpTemplate({
     otp: "482913",
     expiry: 5,
-    name: "Mukal",
-    link: "https://bnbservices.vercel.app/forgot-password",
   });
 
   return new NextResponse(html, {
