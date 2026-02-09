e74e6269cbd6253fda4bec70a47815f4acfb22f3
CodewithMukal <mukal.github@gmail.com>
2026-02-09 18:38:12 +0530

    fix: Quanitity Type UI issue

--- src/app/post/components/QuantityUnitSelector.tsx
+++ src/app/post/components/QuantityUnitSelector.tsx
@@ -22,25 +22,25 @@ const QuantityUnitSelector: NextPage<Props> = ({ quantityUnit, onChange }) => {
   const values = QuantityUnit;
   return (
     <div
-      className="flex flex-col relative z-100"
+      className={`flex flex-col relative px-1 border border-dark/20 z-100 ${expanded ? "rounded-t": "rounded"}`}
       onClick={(e) => {
         e.stopPropagation();
         setExpanded((prev) => !prev);
       }}
     >
-      <div className={`border border-dark/20 cursor-pointer transition-all duration-300 ring-dark flex items-center gap-2 text-dark p-2 h-full ${expanded ? "rounded-t-0 rounded-b-md": "rounded-md"}`}>
+      <div className={`cursor-pointer transition-all duration-300 ring-dark flex items-center gap-5 text-dark p-2 h-full`}>
         {quantityUnit}
         <ChevronDown size={16} className={`${!expanded ? "rotate-180" : ""} transition-all duration-300`} />
       </div>
       {expanded && (
-        <div className={`absolute border border-dark/20 rounded-t border-t-0 bottom-full w-full z-500`}>
+        <div className={`absolute thin-scrollbar left-0 max-h-40 overflow-y-scroll border border-dark/20 rounded-b border-t-0 top-full w-full z-500`}>
           {Object.values(values).map((value) => (
             <div
               key={value}
               onClick={() => {
                 onChange({ quantityUnit: value });
               }}
-              className={`border-t text-center cursor-pointer border-dark/20 z-500 w-full hover:bg-dark hover:text-light transition-all duration-300 flex items-center gap-2 p-2 h-full first:rounded-t ${quantityUnit === value ? "bg-dark text-light" : "bg-white text-dark"}`}
+              className={`border-t text-center cursor-pointer border-dark/20 z-500 w-full hover:bg-dark hover:text-light transition-all duration-300 flex items-center gap-2 p-2 h-full last:rounded-b ${quantityUnit === value ? "bg-dark text-light" : "bg-white text-dark"}`}
             >
               {value}
             </div>
